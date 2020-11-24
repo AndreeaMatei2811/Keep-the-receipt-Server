89359a74637d69057319ff98496e910f1b80c3e2
AndreeaMatei2811 <alm.matei@gmail.com>
2020-11-24 12:41:01 +0100

    set up basic express serer, initialize sequelize

---
+++ index.js
@@ -0,0 +1,8 @@
+const express = require("express");
+const app = express();
+const PORT = 4000;
+
+const User = require("./models").user;
+app.use(express.json());
+
+app.listen(PORT, () => console.log(`Server started in port: ${PORT}`));
