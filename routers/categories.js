f7e68aa844f89975c24857bb2908741df73f0f49
AndreeaMatei2811 <alm.matei@gmail.com>
2020-11-25 19:46:20 +0100

    made route to post a new category

--- routers/categories.js
+++ routers/categories.js
@@ -2,6 +2,7 @@ const { Router } = require("express");
 const auth = require("../auth/middleware");
 const Category = require("../models").category;
 const Product = require("../models").product;
+const User = require("../models").user;
 
 const router = new Router();
 
@@ -16,6 +17,25 @@ router.get("/:id", auth, async (req, res, next) => {
   } catch (e) {
     next(e);
   }
+});
+
+router.post("/:id/newCategory", auth, async (req, res) => {
+  const findUser = await User.findByPk(req.params.id);
+
+  if (!findUser.userId === req.user.id) {
+    return res
+      .status(403)
+      .send({ message: "You are not authorized to make reservation" });
+  }
+  const { name, color } = req.body;
+
+  const newCategory = await Category.create({
+    userId: findUser.id,
+    name,
+    color,
+  });
+
+  return res.status(201).send({ message: "Category created", newCategory });
 });
 
 module.exports = router;
